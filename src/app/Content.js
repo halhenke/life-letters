f2a92bf75725f05b1e3c01eac0cf051af8970b7d
Hal Henke <halhenke@gmail.com>
2015-12-21 00:39:59 +1100

    Content for switching between different views

--- src/app/Content.js
+++ src/app/Content.js
@@ -2,10 +2,38 @@ import React from 'react';
 import styles from './Content.css';
 
 export default class Content extends React.Component {
+
+  userCardClick() {
+    const self = this;
+
+    return (num) => {
+      console.log(`userCardClick got ${num}`);
+      // self.setState({
+      //   component: <DetailView people={users} initial={num} clicked={this.detailClick()} />,
+      // });
+    };
+  }
+
+  detailClick() {
+    const self = this;
+
+    return () => {
+      self.setState({
+        component: <UserCards clicked={this.userCardClick()}/>
+      });
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      component: <UserCards clicked={this.userCardClick()}/>
+    });
+  }
+
   render() {
     return (
       <section className={styles.content}>
-        <h1>Hello, SydJS!</h1>
+        {this.state.component}
       </section>
     );
   }
