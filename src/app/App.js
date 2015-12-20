47e8175b9c97668a740257f4954dc2e208e6c01a
Hal Henke <halhenke@gmail.com>
2015-12-21 00:35:37 +1100

    General Material UI setup

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+injectTapEventPlugin();
 
 export default class App extends React.Component {
   render() {
