2fde8bbad4fc6595db0d7db691c783975cda416d
Hal Henke <halhenke@gmail.com>
2015-12-21 20:02:11 +1100

    Fix header/footer data

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className={styles.footer}>
-        <p>2015 &copy; Nik Butenko</p>
+        <p>2015 &copy; Hal Henke</p>
       </footer>
     );
   }
--- src/app/Header.js
+++ src/app/Header.js
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className={styles.header}>
-        <a className={styles.logo} href="/">Logo</a>
+        <a className={styles.logo} href="/">Life Letters</a>
       </header>
     );
   }
