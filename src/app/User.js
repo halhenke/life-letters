097a5155e713790dba55d821470696bb3c47f178
Hal Henke <halhenke@gmail.com>
2015-12-21 20:26:41 +1100

    avatars for users - a bit cheesy...

--- src/app/User.js
+++ src/app/User.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import classnames from 'classnames';
 import styles from './User.css';
-import {Card, CardHeader, CardMedia, CardTitle} from 'material-ui';
+import {GeneSequence, SequenceKey} from './Genetics.js';
+import {Card, CardHeader, Avatar, CardMedia, CardTitle} from 'material-ui';
+import {Paper} from 'material-ui';
+import Colors from 'material-ui/src/styles/colors';
+import {RaisedButton} from 'material-ui';
+import fb from 'flexboxgrid/dist/flexboxgrid.css';
 
 export const UserList = React.createClass({
 
@@ -12,25 +18,26 @@ export const UserList = React.createClass({
 
   choose(e) {
     e.preventDefault();
-    // console.log(`this.props.click is `, this.props.click);
     this.props.click(this.props.order);
   },
 
   render() {
     const bgStyle = {
-      backgroundImage: `url(${this.props.avatar})`
+      backgroundImage: `url(${this.props.user.avatar})`,
     };
+    const initialAvatar = (
+      <Avatar
+        backgroundColor={(this.props.user.gender === 'female') ? Colors.red400 : Colors.blue400} >
+        {this.props.user.firstName[0]}{this.props.user.lastName[0]}
+      </Avatar>
+    );
+
     return (
       <section className={styles.user} onClick={this.choose}>
         <Card>
-          <CardHeader>
-            {this.props.firstName} {this.props.lastName}
-          </CardHeader>
-          {
-          // <CardMedia overlay={<CardTitle title="User" subtitle="DNA Results"/>}>
-          //   <img src="http://lorempixel.com/600/337/nature/"/>
-          // </CardMedia>
-          }
+          <CardHeader
+            avatar={initialAvatar}
+            title={`${this.props.user.firstName} ${this.props.user.lastName}`} />
           <CardMedia>
             <div className={styles.imgCont}>
               <div className={styles.imgBG} style={bgStyle}>
@@ -63,11 +70,26 @@ export const UserDetail = React.createClass({
 
   render() {
     const fullName = `${this.props.user.firstName} ${this.props.user.lastName}`;
+    const avatarClasses = classnames([
+      fb[`col-xs-6`],
+    ]);
+
     return (
       <div>
         <RaisedButton label="Back to Users" secondary={true} onClick={this.back} />
         <Paper>
           <Card>
+            <div className={fb[`row`]}>
+              <div className={fb[`col-xs-6`]}>
+                <SequenceKey />
+              </div>
+              <div className={avatarClasses}>
+                <CardMedia
+                  overlay={<CardTitle title={fullName} />} >
+                  <img src={this.props.user.avatar}/>
+                </CardMedia>
+              </div>
+            </div>
           </Card>
         </Paper>
       </div>
